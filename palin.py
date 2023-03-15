175cfa71aba838150e8c541e8ce92b6717ae38da
jchen147 <jchen3432@gmail.com>
2023-03-15 06:18:43 -0700

    Problem #1

--- palin.py
+++ palin.py
@@ -4,7 +4,3 @@ def palindrome(lst):
         return True
     else:
         return False
-
-print(palindrome([1,2,"Expresso","Madeline",2,1]))
-print(palindrome(['a',True,False,False,True,'a']))
-print(palindrome([3]))
